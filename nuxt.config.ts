6bbb3b6096ec2ae7abd677b10c8ebf803a3224d6
Vladimir Litvintsev <vl.litvintsev@yandex.ru>
2025-12-14 18:54:01 +0300

    feat: deployment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,20 +1,30 @@
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/image', '@nuxtjs/tailwindcss', '@element-plus/nuxt'],
+  ssr: true,
+  modules: [
+    "@nuxt/eslint",
+    "@nuxt/image",
+    "@nuxtjs/tailwindcss",
+    "@element-plus/nuxt",
+  ],
   runtimeConfig: {
     public: {
-      apiBase: process.env.NUXT_PUBLIC_API_BASE || '/api/v1',
-      filesBase: process.env.NUXT_PUBLIC_FILES_BASE || 'http://localhost:3001'
-    }
+      apiBase:
+        process.env.NUXT_PUBLIC_API_BASE || "http://localhost:3000/api/v1",
+      filesBase: process.env.NUXT_PUBLIC_FILES_BASE || "http://localhost:3001",
+    },
   },
   css: [
-    '~/assets/css/theme.css',
-    '~/assets/css/tailwind.css',
-    'element-plus/dist/index.css',
-    'element-plus/theme-chalk/dark/css-vars.css',
+    "~/assets/css/theme.css",
+    "~/assets/css/tailwind.css",
+    "element-plus/dist/index.css",
+    "element-plus/theme-chalk/dark/css-vars.css",
   ],
   postcss: {
     plugins: { tailwindcss: {}, autoprefixer: {} },
   },
-})
+  devServer: {
+    port: 3001,
+  },
+});
